82be8c58c580c2264f66e2a0ccdcbefb01c13be7
mishafadeeff-jpg <mishafadeeff@gmail.com>
2025-11-16 22:23:16 +0300

    домашняя работа по Lesson1

--- Lesson1/lesson_1_task_2.py
+++ Lesson1/lesson_1_task_2.py
@@ -1,2 +1,4 @@
 my_age = 47
+my_age = my_age + 3
+
 print(my_age)
--- Lesson1/lesson_1_task_3.py
+++ Lesson1/lesson_1_task_3.py
@@ -1,3 +1,5 @@
 first_name = input("Enter your first name: ")
+
 last_name = input("Enter your last name: ")
-print("Вас зовут: ", last_name, first_name )
+
+print("Вас зовут: " + last_name + " " + first_name )
