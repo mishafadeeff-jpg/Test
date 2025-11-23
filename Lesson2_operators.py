927e8b672f223edcea658469c81d4bffa2de0bcc
mishafadeeff-jpg <mishafadeeff@gmail.com>
2025-11-23 09:09:21 +0300

    домашнее задание из ветки lesson2

---
+++ Lesson2_operators.py
@@ -0,0 +1,22 @@
+##user_login = "adam"
+#user_password = "qwerty12345"
+
+#login = input("login: ")
+#password = input("password: ")
+
+#if (login == user_login) and (password == user_password):
+ #   print("Secret is open")
+#else:
+ #   print("Login failed")
+
+crit1 = "red"
+crit2 = "lock"
+
+color = input("color: ")
+feature = input("feature: ")
+
+if (color == crit1) or (feature == crit2):
+    print("Buy it !")
+else:
+    print("Похожу еще посмотрю")
+
